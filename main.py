1e56397ba567f1f665c7c23751c848b684ebba60
emann <60099832@udst.edu.qa>
2023-11-16 13:33:00 +0300

    created a function for  database connection

--- main.py
+++ main.py
@@ -23,17 +23,26 @@ class Recipe:
          self.category =category
          self.rating = rating
 
-class RecipeManagmentSystem:
-    def __init__(self, db_name = 'recipes'):
-        cred = credentials.Certificate("key.json")
-        firebase_admin.initialize_app(cred)
-        self.db = firestore.client()
-        self.collection = self.db.collection(db_name)
+
+def db_connection():
+    cred = credentials.Certificate("key.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    collection = db.collection("recipes").get()
+    recipes_list=[]
+    for r in collection:
+        recipes_list.append(r.to_dict())
+    return recipes_list
+    
+    
 
 @app.route("/")
-def hello_world():
-    print("hello")
+def view_recipes():
+    recipes= db_connection()
+    print(recipes)
     return render_template("home.html")
+
 
 if __name__ == '__main__':
     app.run()
+
